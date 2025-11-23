9fd4957e52116459d710945db1515560109f2431
Vishnu Bharath <mr.vishnubharath@gmail.com>
2025-11-23 01:01:40 -0500

    Initial project setup
    Add all core source files, configuration, licenses, Gradle build scripts, vendor dependencies, and AdvantageScope layout for swerve calibration.

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2026-Robot-Code";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "1131528367a4b2fc82fcb637494bb07187599251";
+  public static final String GIT_DATE = "2025-11-15 20:17:02 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-11-23 01:00:15 EST";
+  public static final long BUILD_UNIX_TIME = 1763877615672L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants() {}
+}
