2f89d06a5aa0bc2053c2adbbee323486643846c0
Vishnu Bharath <mr.vishnubharath@gmail.com>
2025-12-28 21:38:19 -0500

    delete old files on RIO
    remove pathplanner lib files from deploy/

--- src/main/java/org/neiacademy/robotics/BuildConstants.java
+++ src/main/java/org/neiacademy/robotics/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 30;
-  public static final String GIT_SHA = "b0a73ff7ad03e07942ed729dcdbc3d3c3cb11fdd";
-  public static final String GIT_DATE = "2025-12-28 20:28:42 EST";
+  public static final int GIT_REVISION = 31;
+  public static final String GIT_SHA = "eab38cf968740ac8260aaa94f086c014e8b06de2";
+  public static final String GIT_DATE = "2025-12-28 20:41:08 EST";
   public static final String GIT_BRANCH = "sensor-testing";
-  public static final String BUILD_DATE = "2025-12-28 20:40:25 EST";
-  public static final long BUILD_UNIX_TIME = 1766972425514L;
+  public static final String BUILD_DATE = "2025-12-28 21:36:01 EST";
+  public static final long BUILD_UNIX_TIME = 1766975761926L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
