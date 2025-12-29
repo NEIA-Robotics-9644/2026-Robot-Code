b0a73ff7ad03e07942ed729dcdbc3d3c3cb11fdd
Vishnu Bharath <mr.vishnubharath@gmail.com>
2025-12-28 20:28:42 -0500

    quick update to gitignore so that the repo doesn't log the build and gradle files

--- src/main/java/org/neiacademy/robotics/BuildConstants.java
+++ src/main/java/org/neiacademy/robotics/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 27;
-  public static final String GIT_SHA = "5ae973eea65e7537ed474a68baf734bcfdaa31e8";
-  public static final String GIT_DATE = "2025-12-28 20:08:20 EST";
+  public static final int GIT_REVISION = 28;
+  public static final String GIT_SHA = "9df9b4509b0f37d9319fb9c1c23ec4382f9dd95d";
+  public static final String GIT_DATE = "2025-12-28 20:25:51 EST";
   public static final String GIT_BRANCH = "sensor-testing";
-  public static final String BUILD_DATE = "2025-12-28 20:24:45 EST";
-  public static final long BUILD_UNIX_TIME = 1766971485367L;
-  public static final int DIRTY = 128;
+  public static final String BUILD_DATE = "2025-12-28 20:27:16 EST";
+  public static final long BUILD_UNIX_TIME = 1766971636810L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
