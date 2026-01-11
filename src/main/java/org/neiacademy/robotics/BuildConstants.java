db776dd136a5720b24c37e18ae9540e452428364
Vishnu Bharath <mr.vishnubharath@gmail.com>
2026-01-11 15:20:54 -0500

    removed old vendordeps + updated pathplanner lib
    Upgraded PathplannerLib to version 2026.1.1 and removed Phoenix5 and libgrapplefrc2025 vendordep files. Updated BuildConstants to reflect new git revision, branch, and build metadata for the 2026-upgrade branch.

--- src/main/java/org/neiacademy/robotics/BuildConstants.java
+++ src/main/java/org/neiacademy/robotics/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 35;
-  public static final String GIT_SHA = "ccd11995cb2083aa9f53dbada61e01963d14d2f6";
-  public static final String GIT_DATE = "2026-01-10 22:08:57 EST";
-  public static final String GIT_BRANCH = "sensor-testing";
-  public static final String BUILD_DATE = "2026-01-10 22:16:00 EST";
-  public static final long BUILD_UNIX_TIME = 1768101360605L;
+  public static final int GIT_REVISION = 39;
+  public static final String GIT_SHA = "0419de4d61c4206a5134d80d8f49d6fb50ba32da";
+  public static final String GIT_DATE = "2026-01-11 15:11:57 EST";
+  public static final String GIT_BRANCH = "2026-upgrade";
+  public static final String BUILD_DATE = "2026-01-11 15:17:12 EST";
+  public static final long BUILD_UNIX_TIME = 1768162632913L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
