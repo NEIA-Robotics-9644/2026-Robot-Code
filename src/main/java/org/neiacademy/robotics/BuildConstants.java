f11dc95e26b6293f70d111014ecd8a67e018c7a7
Vishnu Bharath <mr.vishnubharath@gmail.com>
2026-01-10 22:17:05 -0500

    simulation for Hall effect sensor IO
    Introduces simulation mode to GenericHallEffectSensorIOImpl, allowing Hall effect sensors to be simulated using a tunable dashboard value via the new HallEffectSim class. Also adds logging of raw and triggered states for better diagnostics.

--- src/main/java/org/neiacademy/robotics/BuildConstants.java
+++ src/main/java/org/neiacademy/robotics/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 34;
-  public static final String GIT_SHA = "7955fb9dc94d9732d5dcff8af93769a741868c1c";
-  public static final String GIT_DATE = "2026-01-08 21:38:52 EST";
+  public static final int GIT_REVISION = 35;
+  public static final String GIT_SHA = "ccd11995cb2083aa9f53dbada61e01963d14d2f6";
+  public static final String GIT_DATE = "2026-01-10 22:08:57 EST";
   public static final String GIT_BRANCH = "sensor-testing";
-  public static final String BUILD_DATE = "2026-01-10 22:03:12 EST";
-  public static final long BUILD_UNIX_TIME = 1768100592495L;
+  public static final String BUILD_DATE = "2026-01-10 22:16:00 EST";
+  public static final long BUILD_UNIX_TIME = 1768101360605L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/org/neiacademy/robotics/lib/genericIO/GenericZeroing/HallEffect/GenericHallEffectSensorIOImpl.java
+++ src/main/java/org/neiacademy/robotics/lib/genericIO/GenericZeroing/HallEffect/GenericHallEffectSensorIOImpl.java
@@ -1,6 +1,7 @@
 package org.neiacademy.robotics.lib.genericIO.GenericZeroing.HallEffect;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import org.littletonrobotics.junction.Logger;
 
 public class GenericHallEffectSensorIOImpl implements GenericHallEffectSensorIO {
 
@@ -15,26 +16,48 @@ public class GenericHallEffectSensorIOImpl implements GenericHallEffectSensorIO 
 
   private final DigitalInput input;
   private final TriggerType triggerType;
-  private String name;
+  private final String name;
+  private final boolean isSim;
+  private final HallEffectSim sim;
 
   /**
-   * @param dioPort DIO port the hall effect sensor is plugged into
-   * @param triggerType Whether the sensor is active-high or active-low
+   * @param constants Configuration for the hall effect sensor (name, port, trigger type)
+   * @param isSim Whether this IO should run in simulation mode
    */
   public GenericHallEffectSensorIOImpl(GenericHallEffectSensorConstants constants, boolean isSim) {
-    name = constants.kName;
-    this.input = new DigitalInput(constants.kDIOport);
+    this.name = constants.kName;
     this.triggerType = constants.ktriggerType;
+    this.isSim = isSim;
+
+    if (isSim) {
+      this.input = null;
+      this.sim = new HallEffectSim(constants.kName);
+    } else {
+      this.input = new DigitalInput(constants.kDIOport);
+      this.sim = null;
+    }
   }
 
   @Override
   public void updateInputs(HallEffectSensorIOInputs inputs) {
-    boolean rawValue = input.get(); // true = HIGH, false = LOW
+    boolean rawValue;
+    boolean triggered;
 
-    inputs.triggered =
-        switch (triggerType) {
-          case ACTIVE_HIGH -> rawValue;
-          case ACTIVE_LOW -> !rawValue;
-        };
+    if (isSim) {
+      triggered = sim.getTriggered();
+      rawValue = triggerType == TriggerType.ACTIVE_HIGH ? triggered : !triggered;
+    } else {
+      rawValue = input.get(); // true = HIGH, false = LOW
+      triggered =
+          switch (triggerType) {
+            case ACTIVE_HIGH -> rawValue;
+            case ACTIVE_LOW -> !rawValue;
+          };
+    }
+
+    inputs.triggered = triggered;
+
+    Logger.recordOutput("HallEffectSensors/IO/" + name + "/Raw", rawValue);
+    Logger.recordOutput("HallEffectSensors/IO/" + name + "/Triggered", triggered);
   }
 }
