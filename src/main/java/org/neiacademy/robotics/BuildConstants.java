ea1ad2e41c10f6dd36cbdbb5c9368e667af96998
Vishnu Bharath <mr.vishnubharath@gmail.com>
2026-01-15 17:16:49 -0500

    Add git graph generator and update documentation
    Introduced assets/git-graph-generator.py to generate SVG visualizations of the repository's git history. Updated README.md to embed the generated git-graph.svg, replacing the previous Graphviz-based SVG. Updated vendordeps and BuildConstants.java with minor changes.

--- src/main/java/org/neiacademy/robotics/BuildConstants.java
+++ src/main/java/org/neiacademy/robotics/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 39;
-  public static final String GIT_SHA = "0419de4d61c4206a5134d80d8f49d6fb50ba32da";
-  public static final String GIT_DATE = "2026-01-11 15:11:57 EST";
+  public static final int GIT_REVISION = 40;
+  public static final String GIT_SHA = "db776dd136a5720b24c37e18ae9540e452428364";
+  public static final String GIT_DATE = "2026-01-11 15:20:54 EST";
   public static final String GIT_BRANCH = "2026-upgrade";
-  public static final String BUILD_DATE = "2026-01-11 15:17:12 EST";
-  public static final long BUILD_UNIX_TIME = 1768162632913L;
+  public static final String BUILD_DATE = "2026-01-15 17:16:14 EST";
+  public static final long BUILD_UNIX_TIME = 1768515374722L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
