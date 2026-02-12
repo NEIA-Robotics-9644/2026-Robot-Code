1ab7e199c92b4ef6465cf14403fad7b0b3dcf75c
Vishnu Bharath <mr.vishnubharath@gmail.com>
2026-02-12 16:21:37 -0500

    Update GradleRIO and vendor library versions
    Bump GradleRIO plugin to 2026.2.1 and update vendordeps to newer releases: PathplannerLib 2026.1.2 (file renamed), CTRE Phoenix6 26.1.1 (file renamed), and photonlib v2026.2.2. Also refresh BuildConstants (git revision/sha/date and build timestamp) to reflect the new build metadata. These changes pull in upstream bug fixes and updated native/java artifacts for the 2026 toolchain.

--- src/main/java/org/neiacademy/robotics/BuildConstants.java
+++ src/main/java/org/neiacademy/robotics/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 52;
-  public static final String GIT_SHA = "20bee6934af4df33f80f4e5f4d81b567933d66ba";
-  public static final String GIT_DATE = "2026-01-22 21:44:12 EST";
+  public static final int GIT_REVISION = 54;
+  public static final String GIT_SHA = "44eccf4793dee818942324fcb89ec4f38d7cdce3";
+  public static final String GIT_DATE = "2026-01-22 22:27:50 EST";
   public static final String GIT_BRANCH = "superstructure";
-  public static final String BUILD_DATE = "2026-01-22 22:22:56 EST";
-  public static final long BUILD_UNIX_TIME = 1769138576213L;
+  public static final String BUILD_DATE = "2026-02-12 16:16:19 EST";
+  public static final long BUILD_UNIX_TIME = 1770930979421L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
