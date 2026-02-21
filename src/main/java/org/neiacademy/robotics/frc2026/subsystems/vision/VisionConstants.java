684a2e025be08784353ff8f63ca5eeeaf5c6fab7
Vishnu Bharath <mr.vishnubharath@gmail.com>
2026-02-21 08:47:42 -0500

    Update VisionConstants.java

--- src/main/java/org/neiacademy/robotics/frc2026/subsystems/vision/VisionConstants.java
+++ src/main/java/org/neiacademy/robotics/frc2026/subsystems/vision/VisionConstants.java
@@ -11,6 +11,7 @@ import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.util.Units;
 
 public class VisionConstants {
   // AprilTag layout
@@ -18,24 +19,23 @@ public class VisionConstants {
       AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
 
   // Camera names, must match names configured on coprocessor
-  public static String camera0Name = "camera_0"; // location
-  public static String camera1Name = "camera_1"; // location
-  public static String camera2Name = "camera_2"; // location
-  public static String camera3Name = "camera_3"; // location
-  public static String camera4Name = "camera_4"; // location
+  public static String camera0Name = "leftcam"; // location
+  public static String camera1Name = "rightcam"; // location
 
   // Robot to camera transforms
   // (Not used by Limelight, configure in web UI instead)
   public static Transform3d robotToCamera0 =
-      new Transform3d(0.2, 0.0, 0.2, new Rotation3d(0.0, -0.4, 0.0));
+      new Transform3d(
+          Units.inchesToMeters(-11.2),
+          Units.inchesToMeters(2.8),
+          Units.inchesToMeters(5),
+          new Rotation3d(0.0, Math.toRadians(10), Math.toRadians(-110)));
   public static Transform3d robotToCamera1 =
-      new Transform3d(-0.2, 0.0, 0.2, new Rotation3d(0.0, -0.4, Math.PI));
-  public static Transform3d robotToCamera2 =
-      new Transform3d(-0.2, 0.0, 0.2, new Rotation3d(0.0, -0.4, Math.PI));
-  public static Transform3d robotToCamera3 =
-      new Transform3d(-0.2, 0.0, 0.2, new Rotation3d(0.0, -0.4, Math.PI));
-  public static Transform3d robotToCamera4 =
-      new Transform3d(0.0, 0.0, 0.0, new Rotation3d(0.0, 0.0, Math.PI));
+      new Transform3d(
+          Units.inchesToMeters(-11.2),
+          Units.inchesToMeters(-2.8),
+          Units.inchesToMeters(5),
+          new Rotation3d(0.0, Math.toRadians(10), Math.toRadians(110)));
 
   // Basic filtering thresholds
   public static double maxAmbiguity = 0.3;
