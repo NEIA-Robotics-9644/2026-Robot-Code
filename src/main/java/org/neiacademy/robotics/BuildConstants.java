916867c8312b1491c90f599bdf32b812825670a1
hydrobyte-x <hydrobyte.dev@gmail.com>
2026-02-20 20:36:25 -0500

    Update BuildConstants.java

--- src/main/java/org/neiacademy/robotics/BuildConstants.java
+++ src/main/java/org/neiacademy/robotics/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 50;
-  public static final String GIT_SHA = "40d241a816e36fb5a2741a428e955899460f9ed6";
-  public static final String GIT_DATE = "2026-02-20 18:14:43 EST";
+  public static final int GIT_REVISION = 52;
+  public static final String GIT_SHA = "0ae85d28ef8b7d3aeeed8d62b5cfc34f2e61e3de";
+  public static final String GIT_DATE = "2026-02-20 20:36:12 EST";
   public static final String GIT_BRANCH = "shooter";
-  public static final String BUILD_DATE = "2026-02-20 18:14:56 EST";
-  public static final long BUILD_UNIX_TIME = 1771629296246L;
+  public static final String BUILD_DATE = "2026-02-20 20:36:20 EST";
+  public static final long BUILD_UNIX_TIME = 1771637780560L;
   public static final int DIRTY = 0;
 
   private BuildConstants() {}
