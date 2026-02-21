d96b926477e5048782756be23d954d69c0f279b3
Vishnu Bharath <mr.vishnubharath@gmail.com>
2026-02-21 08:39:19 -0500

    new tuner constants

--- src/main/java/org/neiacademy/robotics/BuildConstants.java
+++ src/main/java/org/neiacademy/robotics/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 74;
-  public static final String GIT_SHA = "0f60e5234cab72bc937872804f968277b66236b8";
-  public static final String GIT_DATE = "2026-02-21 08:25:40 EST";
-  public static final String GIT_BRANCH = "intakeCancoder";
-  public static final String BUILD_DATE = "2026-02-21 08:28:11 EST";
-  public static final long BUILD_UNIX_TIME = 1771680491451L;
+  public static final int GIT_REVISION = 77;
+  public static final String GIT_SHA = "ffd52f8374b76ba8b9e6e27426b3d60f59b26b2c";
+  public static final String GIT_DATE = "2026-02-21 08:30:37 EST";
+  public static final String GIT_BRANCH = "event-week0";
+  public static final String BUILD_DATE = "2026-02-21 08:34:18 EST";
+  public static final long BUILD_UNIX_TIME = 1771680858228L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
