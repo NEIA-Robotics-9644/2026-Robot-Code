88cd169ab949fcbee4aafa5757dfad0c686db7f1
Vishnu Bharath <mr.vishnubharath@gmail.com>
2026-05-29 21:50:09 -0400

    update brownout voltage

--- src/main/java/org/neiacademy/robotics/frc2026/RobotContainer.java
+++ src/main/java/org/neiacademy/robotics/frc2026/RobotContainer.java
@@ -17,6 +17,7 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.Alert.AlertType;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -105,6 +106,9 @@ public class RobotContainer {
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+
+    RobotController.setBrownoutVoltage(6.0);
+
     switch (Constants.currentMode) {
       case REAL:
         // Real robot, instantiate hardware IO implementations
