80c613e54044dfbbf1f70c9bde244acc787959ab
Vishnu Bharath <mr.vishnubharath@gmail.com>
2026-06-19 15:10:20 -0400

    phoebe cad model init for sim

--- src/main/java/org/neiacademy/robotics/BuildConstants.java
+++ src/main/java/org/neiacademy/robotics/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 273;
-  public static final String GIT_SHA = "67225bd8e82d76a0764604255f57f6bd2e474596";
-  public static final String GIT_DATE = "2026-06-06 15:44:05 EDT";
+  public static final int GIT_REVISION = 278;
+  public static final String GIT_SHA = "2f3222a48447d877385f3f71d11f239d6fbc9938";
+  public static final String GIT_DATE = "2026-06-06 23:39:06 EDT";
   public static final String GIT_BRANCH = "event-battlecry";
-  public static final String BUILD_DATE = "2026-06-06 15:44:06 EDT";
-  public static final long BUILD_UNIX_TIME = 1780775046753L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2026-06-19 15:01:45 EDT";
+  public static final long BUILD_UNIX_TIME = 1781895705328L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
